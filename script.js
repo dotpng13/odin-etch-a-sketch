3fef62a39bc262ae316e1cf234c640d1dd0a2680
Quinn Siebers <packerfan21396@gmail.com>
2023-04-20 15:15:51 -0500

    Remove grid cell gaps, calculate cell size
    Remove the gaps between the cells in order to improve functionality at
    smaller window sizes.  Script the calculation of cell sizes in order to
    generate different size grids at a similar total area.

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 const container = document.querySelector("#container");
 
-function generateGrid (numRows, numCols) {
-    let row, col;
+function generateGrid (numSide) {
+    const idealContainerSide = 512;
+    const cellSide = Math.floor(idealContainerSide / numSide);
+    const rowFlexBasis = cellSide - 2;
+    const cellFlexBasis = cellSide - 4;
+    let row, cell;
     
-    for (let i = 0; i < numRows; i++) {
+    for (let i = 0; i < numSide; i++) {
         row = document.createElement("div");
         row.classList.add("row");
-        for (let j = 0; j < numCols; j++) {
-            col = document.createElement("div");
-            col.classList.add("cell");
-            row.appendChild(col);
+        row.style.flexBasis = `${rowFlexBasis}px`;
+        for (let j = 0; j < numSide; j++) {
+            cell = document.createElement("div");
+            cell.classList.add("cell");
+            cell.style.flexBasis = `${cellFlexBasis}px`;
+            row.appendChild(cell);
         }
         container.appendChild(row);
     }
 }
 
-generateGrid(16, 16);
+generateGrid(16);

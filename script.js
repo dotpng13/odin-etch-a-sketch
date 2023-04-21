3ce8c93b227e0c3954c46fd25f2b912ed93835ab
Quinn Siebers <packerfan21396@gmail.com>
2023-04-21 11:07:59 -0500

    Add reset button, refine grid generation
    Add a reset button and subsequent prompts to resize the grid after the
    reset.  Increase the size of the drawing area and calculate the cell
    size via rounding rather than the floor function to better fill the
    drawing area.  Add helper functions in the script for better
    readability.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const container = document.querySelector("#container");
 
-function generateGrid (numSide) {
-    const idealContainerSide = 512;
-    const cellSide = Math.floor(idealContainerSide / numSide);
+function generateGrid(numSide) {
+    const idealContainerSide = 960;
+    const cellSide = Math.round(idealContainerSide / numSide);
     const rowFlexBasis = cellSide - 2;
     const cellFlexBasis = cellSide - 4;
     let row, cell;
@@ -21,11 +21,43 @@ function generateGrid (numSide) {
     }
 }
 
-generateGrid(16);
+function resetGrid() {
+    let newSide = prompt("What side length grid would you like?");
+    newSide = Number(newSide);
 
-const cells = document.querySelectorAll(".cell");
-cells.forEach((cell) => {
-    cell.addEventListener("mouseenter", () => {
-        cell.style.backgroundColor = "black";
+    while(true) {
+        if (newSide > 100) {
+            newSide = prompt("ERROR: the number you entered is too big!"
+                + "\nWhat side length grid would you like?");
+        } else if (newSide < 1) {
+            newSide = prompt("ERROR: the number you entered is too small!"
+                + "\nWhat side length grid would you like?");
+        } else if (newSide % 1 !== 0) {
+            newSide = prompt("ERROR: the number you entered is not an integer!"
+                + "\nWhat side length grid would you like?");
+        } else {
+            break;
+        }
+    }
+
+    while (container.firstElementChild) {
+        container.firstElementChild.remove();
+    }
+    start(newSide);
+}
+
+function beginDrawing() {
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => {
+        cell.addEventListener("mouseenter", () => {
+            cell.style.backgroundColor = "black";
+        });
     });
-});
+}
+
+function start(numSide) {
+    generateGrid(numSide);
+    beginDrawing();
+}
+
+start(16);
